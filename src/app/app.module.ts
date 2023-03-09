2f2b1d3921221887689275f30d0ee428ae36f8bf
fayas8086 <fayasjabbar12345@gmail.com>
2023-03-09 20:33:33 +0530

    basic model template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
+import { TopnavbarComponent } from './topnavbar/topnavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SidenavbarComponent,
+    TopnavbarComponent
   ],
   imports: [
     BrowserModule
